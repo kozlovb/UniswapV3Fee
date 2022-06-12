ac54a65934128c7726a0b3137172b9767ef70628
Boris Kozlov <bkozlov@C02X84NSJGH6.ad.here.com>
2022-06-12 12:43:12 +0200

    Added data from https://defi-lab.xyz/uniswapv3simulator for self check

--- request_fee.py
+++ request_fee.py
@@ -2,19 +2,48 @@ import requests
 import json
 import pandas as pd
 from find_closest_tick import *
+import math
+"""
+Jun 11, 14.50.       - 30 days.     13 May 14.50
+# check data
+Block now 14944320
+Block 30 days ago
+14767479
+
+WETH
+0.03111
+USDT
+57.18
+
+You can get the block number by timestamp, using etherscan's API:
+https://www.epochconverter.com
+https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=1652446239&closest=before&apikey=ABRCM9H8AIM911I5H7GNGDU9EJU53YCGCN
+
+pool id
+"""
+
+
+
 
 url = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 
-#  0x000ea4a83acefdd62b1b43e9ccc281f442651520  {'symbol': 'BUSD'}   {'symbol': 'WETH'}    5508168315607950206  4794438.262787290195418958         1229.60354080782795285
+ETH_decimals  = 18
+USDT_decimals  = 6
 # data about pool 0x4e68ccd3e89f51c3074ca5072bbac773960dfa36
 # https://info.uniswap.org/#/
-# -195807 may be current tick
 
-#def tick_to_price(tick)
-# examplerecent ic is -201039
+#data about token like name decimals ..
+# https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3/graphql?query=query+MyQuery+%7B%0A++pool%28id%3A+%220x4e68ccd3e89f51c3074ca5072bbac773960dfa36%22%29+%7B%0A++++token0+%7B%0A++++++symbol%0A++++++decimals%0A++++%7D%0A++%7D%0A%7D
+
+def tick_to_price(tick, decimals):
+    return pow(1.0001,tick) / pow(10, decimals)
+def tick_to_price_ETH_in_USD(tick):
+    return pow(1.0001,tick)*pow(10, ETH_decimals-USDT_decimals)
+def price_to_tick(price, decimals):
+    math.log(price, 1.001)/pow(10, decimals)
+# example present tick ic is -201039
 # check https://atiselsts.github.io/pdfs/uniswap-v3-liquidity-math.pdf one needs to add 10^12 i.e.
-#  i = log(base : 1.001, ETHprice/10^12)
-#  ETHprice = 10^12 * 1.0001 ^ i
+
 
 
 # -196256 3k
@@ -37,20 +66,21 @@ def calculate_fr(tickup, ticklow, tick1_fee_Outside, tick2_fee_Outside, ic, glob
     if ic < tickup:
         feeGrowthAbove0X128 = tick1_fee_Outside
     else:
-        feeGrowthAbove0X128 = feeGrowthGlobal0X128 - tick1_fee_Outside
+        feeGrowthAbove0X128 = global_fee - tick1_fee_Outside
     feeGrowthInside0X128 = global_fee - feeGrowthBelow0X128 - feeGrowthAbove0X128
     return feeGrowthInside0X128
 
 def to_regular_numbers(number_128, decimal):
     return number_128/(pow(2,128)*pow(10, decimal))
 #2400 mas o menos -200000
+
+
 tick_up = -199980
-old_block = 14914928
-new_block = 14916928
+
+old_block = 14767479   # (Jun-06-2022 12:13:20 PM +UTC)
+new_block = 14944320   # (Jun-06-2022 08:33:23 PM +UTC)
 tick_up_old = requests.post(url, json={'query': query(old_block, tick_up)})
 tick_up_new = requests.post(url, json={'query': query(block=new_block, tick = tick_up)})
-print(tick_up_old.text)
-#print(tick_up_new.text)
 
 json_data = json.loads(tick_up_old.text)
 tickup_fee_Outside_old = int(json_data["data"]["ticks"][0]["feeGrowthOutside0X128"])
@@ -71,14 +101,12 @@ ticklow_fee_Outside_old = int(json_data["data"]["ticks"][0]["feeGrowthOutside0X1
 json_data = json.loads(tick_low_new.text)
 ticklow_fee_Outside_new = int(json_data["data"]["ticks"][0]["feeGrowthOutside0X128"])
 #print("Found this results", tickup_fee_Outside_old, ic_old, global_fee_old)
-liquidity  = 1
+liquidity  = 250
 fee = calculate_fee(tick_up, tick_low, tickup_fee_Outside_old, ticklow_fee_Outside_old, ic_old, global_fee_old, tickup_fee_Outside_new, ticklow_fee_Outside_new, ic_new, global_fee_new, liquidity)
 print("RESULT", to_regular_numbers(fee, 18))
 
+# 2067.346 0.00048371337
+# 1685.86 0.000593169
 
-#print(json_data)
-
-#calculate_fee(tick1, tick2, tick1_old_json[][])
-
-#df = pd.DataFrame(json_data["data"]["pools"])
-#print(df)
+print("price at tick up", tick_to_price_ETH_in_USD(tick_up))
+print("price at tick low", tick_to_price_ETH_in_USD(tick_low))
